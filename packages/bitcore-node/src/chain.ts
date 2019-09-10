NandyShi/bitcore

Add LTC support

packages/bitcore-node/src/chain.ts
@@ -7,4 +7,8 @@ module.exports = {
     lib: require('bitcore-lib-cash'),
     p2p: require('bitcore-p2p-cash'),
   },
+  LTC: {
+    lib: require('bitcore-lib'),
+    p2p: require('litecore-p2p'),
+  }
 }

cwcrypto/bitcore-1

packages/bitcore-node/src/chain.ts
@@ -7,4 +7,8 @@ module.exports = {
     lib: require('bitcore-lib-cash'),
     p2p: require('bitcore-p2p-cash'),
   },
+  LTC: {
+    lib: require('litecore-lib'),
+    p2p: require('litecore-p2p'),
+  }
 }